5702b312d4225844af1dc804670cc42cc0a8474a
Utsav-83 <utsav8391@gmail.com>
2025-10-28 11:56:34 +0530

    Student Details

---
+++ studentdetails.py
@@ -0,0 +1,13 @@
+name = "Ace"
+usn = "01BCAXYZ123"
+branch = "BCA"
+sem = 3
+clg = "ABC College"
+
+print("Student Details:")
+print("Name:", name)        
+print("USN:", usn)
+print("Branch:", branch)
+print("Semester:", sem)
+print("College:", clg)
+
